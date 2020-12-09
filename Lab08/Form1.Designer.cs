620c881cd38a168d901c747d69d95bdb6b33c9a7
StatTrak <wireless.00.d@gmail.com>
2020-12-09 15:14:41 +0200

    Добавьте файлы проекта.

---
+++ Lab08/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace Lab08
+{
+    partial class PrinterInfo
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPrinterInfo = new System.Windows.Forms.TextBox();
+            this.btnAddPrinter = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbPrinterInfo
+            // 
+            this.tbPrinterInfo.Location = new System.Drawing.Point(46, 57);
+            this.tbPrinterInfo.Multiline = true;
+            this.tbPrinterInfo.Name = "tbPrinterInfo";
+            this.tbPrinterInfo.ReadOnly = true;
+            this.tbPrinterInfo.Size = new System.Drawing.Size(519, 331);
+            this.tbPrinterInfo.TabIndex = 0;
+            this.tbPrinterInfo.TextChanged += new System.EventHandler(this.tbPrinterInfo_TextChanged);
+            // 
+            // btnAddPrinter
+            // 
+            this.btnAddPrinter.Location = new System.Drawing.Point(610, 57);
+            this.btnAddPrinter.Name = "btnAddPrinter";
+            this.btnAddPrinter.Size = new System.Drawing.Size(110, 23);
+            this.btnAddPrinter.TabIndex = 1;
+            this.btnAddPrinter.Text = "Додати принтер";
+            this.btnAddPrinter.UseVisualStyleBackColor = true;
+            this.btnAddPrinter.Click += new System.EventHandler(this.btnAddPrinter_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(610, 365);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(110, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Закрити";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // PrinterInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 450);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnAddPrinter);
+            this.Controls.Add(this.tbPrinterInfo);
+            this.MaximizeBox = false;
+            this.Name = "PrinterInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторна робота №8";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPrinterInfo;
+        private System.Windows.Forms.Button btnAddPrinter;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ Lab08/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab08
+{
+    public partial class PrinterInfo : Form
+    {
+        public PrinterInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddPrinter_Click(object sender, EventArgs e)
+        {
+
+
+            //Printer printer = new Printer(printerName, pDPI, pPaperSize, pSpeedWrite, pWiFi, pTexWrite, printerLife);
+            double DPI =  double.Parse("1,1");
+            Printer printer = new Printer("",DPI,"",0.0, false, Printer.TextWriteState.Inkjet,0.0);
+            PrinterForm pf = new PrinterForm(printer);
+            if (pf.ShowDialog() == DialogResult.OK) 
+            {
+                tbPrinterInfo.Text += string.Format("Назва принтеру: {0}", pf.ThePrinter.printerName);
+                tbPrinterInfo.Text += string.Format("Якість друку: {0}", pf.ThePrinter.pDPI);
+                tbPrinterInfo.Text += string.Format("Рормір паперу: {0}", pf.ThePrinter.pPaperSize);
+                tbPrinterInfo.Text += string.Format(" Швидкіть друку: {0}", pf.ThePrinter.pSpeedWrite);
+                tbPrinterInfo.Text += string.Format(" Підтримка WiFi: {0}", pf.ThePrinter.pWiFi);
+                tbPrinterInfo.Text += string.Format(" Тип печаті: {0}", pf.ThePrinter.pTexWrite);
+                tbPrinterInfo.Text += string.Format(" Строк придатності: {0}", pf.ThePrinter.printerLife);
+                
+
+
+
+
+                // tbPrinterInfo.Text += string.Format("Назва принтеру: {0},\n Якість друку: {1},\n Рормір паперу: {2}.\n Швидкіть друку: { 3}.\n", pf.ThePrinter., pf.ThePrinter.pDPI, pf.ThePrinter.pSpeedWrite);
+            }
+        }
+
+        private void tbPrinterInfo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+                            if (MessageBox.Show("Припинити роботу застосунку?",
+                "Припинити роботу", MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Question) == DialogResult.OK)
+                                Application.Exit();
+        }
+    }
+}
